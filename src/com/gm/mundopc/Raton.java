35b2a4a8097acbd11653f9883e449d7f05a09aaf
Didier Ramirez <didierjavierr@gmail.com>
2022-03-17 13:18:18 -0500

    Se crea la clase Dispositivo de mouse, que hereda de la clase dispositivo de entrada

---
+++ src/com/gm/mundopc/Raton.java
@@ -0,0 +1,21 @@
+package com.gm.mundopc;
+
+/**
+ *
+ * @author DIDIER JAVIER
+ */
+public class Raton extends DispositivoEntrada{
+    private int idRaton;
+    private static int contadorRatones;
+
+    public Raton(String tipoEntrada, String marca) {
+        super(tipoEntrada, marca);
+        this.idRaton = Raton.contadorRatones++;
+    }
+
+    @Override
+    public String toString() {
+        return "Raton{" + "idRaton=" + idRaton + '}';
+    }    
+    
+}
